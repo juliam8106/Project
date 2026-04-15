c3cd506d80e31e412b1c837448dbff3300b85780
Julia Misiura <Julia.Misiura@mymtu.ie>
2026-04-15 13:36:42 +0100

    Added English.txt file and fixed formating

--- useful_functions.py
+++ useful_functions.py
@@ -1,22 +1,23 @@
 print("useful_functions.py", " __name__ is", __name__)
 def get_non_empty_string(prompt):
-while True:
-    answer = input(prompt)
-    if len(answer) > 0 :
-    break
+    while True:
+        answer = input(prompt)
+        if len(answer) > 0 :
+            break
     return answer
 
 def get_positive_int(prompt):
-while True:
-    try:
-        num = int(input(prompt))
-        if num >= 0:
-        break
-    except:
-        print("Error occured")
+    while True:
+        try:
+            num = int(input(prompt))
+            if num >= 0:
+                break
+        except:
+            print("Error occured")
     return num
+
 def main():
-print("Hello")
+    print("Hello")
 # TEst all functions
 if __name__ == "__main__":
-main()
+    main()
